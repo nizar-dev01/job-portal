c28c41931e4e24e477e758d1634aa70cd73ed06f
Nizar K <nizar.dev01@gmail.com>
2020-12-14 11:11:31 +0530

    data updation

--- src/store/modules/data.js
+++ src/store/modules/data.js
@@ -8,6 +8,8 @@
                         name: 'Nizar',
                         email: 'nizar.dev01@gmail.com',
                         password: 'password',
+                        phone: '+91 768584732134',
+                        address: 'Address Line 1, Example Street',
                         type: 0,
                         id: 1
                     },
@@ -15,6 +17,8 @@
                         name: 'Jason',
                         email: 'jason@gmail.com',
                         password: 'password',
+                        phone: '+91 768584732134',
+                        address: 'Address Line 1, Example Street',
                         type: 1,
                         id: 2
                     },
@@ -22,6 +26,8 @@
                         name: 'Thomas',
                         email: 'thomas@gmail.com',
                         password: 'password',
+                        phone: '+91 768584732134',
+                        address: 'Address Line 1, Example Street',
                         type: 1,
                         id: 3
                     },
@@ -29,6 +35,8 @@
                         name: 'Melissa',
                         email: 'melissa@gmail.com',
                         password: 'password',
+                        phone: '+91 768584732134',
+                        address: 'Address Line 1, Example Street',
                         type: 1,
                         id: 4
                     }
@@ -43,8 +51,7 @@
                         description: "<p>We're looking for a front end developer for our company to help with creating a slick modern web front end. The job requires experience with the javascript ecosystem (primarily Vue.js), HTML, CSS, and related tooling. Experience with UI/UX principles and design is an added bonus. Open-source contributions and public activity (e.g. hackerrank, stack overflow, etc.) related to front-end technologies is a big bonus.</p><p>You will be in charge of developing the front end for all our products using the above-mentioned libraries. Your code should be highly performant, well documented, tested, and cleanly structured. You should mentor and help junior engineers come up to speed and over time, grow into a technical leadership role. We're working remotely now so you'll need a good net connection, familiarity with Linux, and a computer. Good written and spoken communication is vital.</p><p class=\"ql-indent-1\"><br></p><p class=\"ql-indent-1\"><strong>Call at</strong> +91 93243546574</p><p class=\"ql-indent-1\"><strong>Job Type: </strong>Full-time</p><p class=\"ql-indent-1\"><strong>Salary:</strong> ₹11,993.00 - ₹37,256.00 per month</p><p><br></p><p><strong>Schedule:</strong></p><ul><li>Monday to Friday</li></ul><p><strong>Work Remotely:</strong></p><ul><li>Temporarily due to COVID-19</li></ul>",
                         hidden: false,
                         slug: 'job-1-slug',
-                        likedBy:[],
-                        applications: []
+                        likedBy:[]
                     },
                     {
                         id: 2,
@@ -55,8 +62,7 @@
                         description: "<p>We're looking for a front end developer for our company to help with creating a slick modern web front end. The job requires experience with the javascript ecosystem (primarily Vue.js), HTML, CSS, and related tooling. Experience with UI/UX principles and design is an added bonus. Open-source contributions and public activity (e.g. hackerrank, stack overflow, etc.) related to front-end technologies is a big bonus.</p><p>You will be in charge of developing the front end for all our products using the above-mentioned libraries. Your code should be highly performant, well documented, tested, and cleanly structured. You should mentor and help junior engineers come up to speed and over time, grow into a technical leadership role. We're working remotely now so you'll need a good net connection, familiarity with Linux, and a computer. Good written and spoken communication is vital.</p><p class=\"ql-indent-1\"><br></p><p class=\"ql-indent-1\"><strong>Call at</strong> +91 93243546574</p><p class=\"ql-indent-1\"><strong>Job Type: </strong>Full-time</p><p class=\"ql-indent-1\"><strong>Salary:</strong> ₹11,993.00 - ₹37,256.00 per month</p><p><br></p><p><strong>Schedule:</strong></p><ul><li>Monday to Friday</li></ul><p><strong>Work Remotely:</strong></p><ul><li>Temporarily due to COVID-19</li></ul>",
                         hidden: false,
                         slug: 'job-2-slug',
-                        likedBy:[],
-                        applications: []
+                        likedBy:[]
                     },
                     {
                         id: 3,
@@ -67,9 +73,22 @@
                         description: "<p>We're looking for a front end developer for our company to help with creating a slick modern web front end. The job requires experience with the javascript ecosystem (primarily Vue.js), HTML, CSS, and related tooling. Experience with UI/UX principles and design is an added bonus. Open-source contributions and public activity (e.g. hackerrank, stack overflow, etc.) related to front-end technologies is a big bonus.</p><p>You will be in charge of developing the front end for all our products using the above-mentioned libraries. Your code should be highly performant, well documented, tested, and cleanly structured. You should mentor and help junior engineers come up to speed and over time, grow into a technical leadership role. We're working remotely now so you'll need a good net connection, familiarity with Linux, and a computer. Good written and spoken communication is vital.</p><p class=\"ql-indent-1\"><br></p><p class=\"ql-indent-1\"><strong>Call at</strong> +91 93243546574</p><p class=\"ql-indent-1\"><strong>Job Type: </strong>Full-time</p><p class=\"ql-indent-1\"><strong>Salary:</strong> ₹11,993.00 - ₹37,256.00 per month</p><p><br></p><p><strong>Schedule:</strong></p><ul><li>Monday to Friday</li></ul><p><strong>Work Remotely:</strong></p><ul><li>Temporarily due to COVID-19</li></ul>",
                         hidden: false,
                         slug: 'job-3-slug',
-                        likedBy:[],
-                        applications: []
+                        likedBy:[]
                     },
+                ],
+                applications:[
+                    {
+                        id: 1,
+                        slug: 'app-1-slug',
+                        name: 'Muhammed Nizar K',
+                        email: 'nizar.example@gmail.com',
+                        phone: '+91 456378213509',
+                        address: 'Koduvally, Calicut, Kerala',
+                        resume: 'som.link/file',
+                        cover:'<h1>This is a Cover Letter</h1>',
+                        job_slug: 'job-1-slug',
+                        job_by: 1
+                    }
                 ],
                 loggedIn: false
             }
@@ -97,9 +116,11 @@
                 return new Promise((res,rej)=>{
                     let
                         error,
-                        success =state.users.some(u=>{
+                        success = state.users.some(u=>{
                             if(u.email == email && u.password == password){
                                 commit('login',u)
+                                u = JSON.stringify(u)
+                                localStorage.setItem('auth_user',u)
                                 return true
                             }else if(u.email == email || u.password == password){
                                 error = 1
@@ -126,7 +147,10 @@
                     }
                 })
             },
-            logout({commit}){ commit('logout') },
+            logout({commit}){
+                commit('logout')
+                localStorage.removeItem('auth_user')
+            },
             getjobs({state}){
                 return new Promise((res)=>{
                     delay(res,{
@@ -165,8 +189,7 @@
                                 id,
                                 slug: `job-${id}-slug`,
                                 hidden: false,
-                                likedBy: [],
-                                applications: []
+                                likedBy: []
                             }
                         commit('createNewJob',newJob)
                         delay(res,{
@@ -197,23 +220,60 @@
                 })
                 else target.likedBy.push(user)
             },
-            getApplications({state},user){
+            getApplications({state}){
+                let apps = state.applications
                 return new Promise((res,rej)=>{
-                    const apps = [1,1,1]
-                    state.jobs.forEach( j => {
-                        if(j.created_by===user)
-                        j.applications.forEach(ap=>apps.push(ap))
-                    
-                    })
-                    if(apps.length) delay(res,{
+                    if(apps.length){
+                        apps.forEach(ap=>{
+                            const job = state.jobs.find(j=>j.slug==ap.job_slug)
+                            if(job) ap.job = job
+                        })
+                        delay(res,{
                             status: 200,
                             data: apps
                         })
+                    }
                     else delay(rej,{
                         status: 404,
                         message: 'No Applications Recieved Yet!'
                     })
                 })
+            },
+            getApp({state},slug){
+                return new Promise((res,rej)=>{
+                    const data = state.applications.find(a=>a.slug==slug)
+                    if(data){
+                        const job = state.jobs.find(j=>j.slug==data.job_slug)
+                        data.job = job
+                        delay(res,{
+                            status: 200,
+                            data
+                        })
+                    }
+                    else delay(rej,{
+                        status: 404,
+                        message: 'No Application Found'
+                    })
+                })
+            },
+            applyForJob({state},app){
+                return new Promise(res=>{
+                    const
+                        { applications , jobs } = state,
+                        { job_slug } = app,
+                        slug = `app-${jobs.length+1}-slug`,
+                        job_by = jobs.find(j=>j.slug==job_slug).created_by
+                    app.job_by = job_by
+                    app.slug = slug
+                    applications.push(app)
+                    delay(res,{
+                        status: 200
+                    })
+                })
+            },
+            S_init({dispatch}){
+                const user = localStorage.getItem('auth_user')
+                if(user) dispatch('login',JSON.parse(user))
             }
         },
         getters:{
@@ -221,7 +281,9 @@
             // loggedIn: ()=>true,
             userType: s => s.loggedIn ? s.loggedIn.type : null,
             // userType: () => 0,
-            userId: s => s.loggedIn ? s.loggedIn.id : null
+            userId: s => s.loggedIn ? s.loggedIn.id : null,
+            applications: s => s.applications,
+            user: s => s.loggedIn
         }
     }
 
