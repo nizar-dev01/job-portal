669c7c70699c327615d3a500396beffdd0f5af0a
Nizar K <nizar.dev01@gmail.com>
2020-12-14 11:11:15 +0530

    auto dispatch

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,11 @@ import modules from './modules.js'
 const store = createStore({ modules })
 
 // Store Initialization when the app loads
-if( modules.init && modules.auth ){
-  if(modules.init.actions && modules.auth.actions){
-    // general actions for auth and unauth
-    for(const action in modules.init.actions){
-      const pass = /^G_/.test(action)
-      if(pass){
-        store.dispatch('init/'+action)
-      }
+for(let module in modules){
+  for(let action in modules[module].actions){
+    const pass = /^S_/.test(action)
+    if(pass){
+      store.dispatch('data/'+action)
     }
   }
 }
